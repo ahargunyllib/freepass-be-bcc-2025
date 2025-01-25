b5e88e2aabe8918533ca06c2b50f3272ce8712dc
ahargunyllib <billy.bpm03@gmail.com>
2025-01-25 17:21:44 +0700

    feat: Update CreateUser method to return error instead of GetUserResponse

--- domain/contracts/user_contract.go
+++ domain/contracts/user_contract.go
@@ -19,6 +19,6 @@ type UserRepository interface {
 type UserService interface {
 	GetUsers(ctx context.Context, query dto.GetUsersQuery) (dto.GetUsersResponse, error)
 	GetUser(ctx context.Context, query dto.GetUserQuery) (dto.GetUserResponse, error)
-	CreateUser(ctx context.Context, req dto.CreateUserRequest) (dto.GetUserResponse, error)
+	CreateUser(ctx context.Context, req dto.CreateUserRequest) error
 	DeleteUser(ctx context.Context, query dto.DeleteUserQuery) error
 }
