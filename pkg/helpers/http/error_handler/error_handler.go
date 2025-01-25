068ba2e0d3e82a80e69bdcd9eeafde01d150bffc
ahargunyllib <billy.bpm03@gmail.com>
2025-01-25 15:43:48 +0700

    feat: Add validation functionality using go-playground/validator and error handling improvements

--- pkg/helpers/http/error_handler/error_handler.go
+++ pkg/helpers/http/error_handler/error_handler.go
@@ -5,10 +5,16 @@ import (
 
 	"github.com/ahargunyllib/freepass-be-bcc-2025/domain"
 	"github.com/ahargunyllib/freepass-be-bcc-2025/pkg/helpers/http/response"
+	"github.com/ahargunyllib/freepass-be-bcc-2025/pkg/validator"
 	"github.com/gofiber/fiber/v2"
 )
 
 func ErrorHandler(c *fiber.Ctx, err error) error {
+	var valErr validator.ValidationErrors
+	if errors.As(err, &valErr) {
+		return response.SendResponse(c, fiber.StatusUnprocessableEntity, valErr)
+	}
+
 	var reqErr *domain.RequestError
 	if errors.As(err, &reqErr) {
 		return response.SendResponse(c, reqErr.StatusCode, reqErr)
